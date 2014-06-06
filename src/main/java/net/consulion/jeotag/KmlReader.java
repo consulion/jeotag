24018515193ad7cba5d64d7f2819108e55c6be20
unknown <ole.roessler@SYDNEY.OT>
2014-06-06 16:54:08 +0200

    progress

--- src/main/java/net/consulion/jeotag/KmlReader.java
+++ src/main/java/net/consulion/jeotag/KmlReader.java
@@ -16,25 +16,73 @@
  */
 package net.consulion.jeotag;
 
-import de.micromata.opengis.kml.v_2_2_0.Document;
-import de.micromata.opengis.kml.v_2_2_0.Feature;
-import de.micromata.opengis.kml.v_2_2_0.Kml;
-import de.micromata.opengis.kml.v_2_2_0.Placemark;
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+import java.util.ArrayList;
+import java.util.List;
+import net.consulion.jeotag.model.LocationRecord;
 
 public class KmlReader {
 
     public static void read(final File file) {
-        final Kml kml = Kml.unmarshal(file);
-        final Feature feature = kml.getFeature();
-        if (feature instanceof Document) {
-            final Document document = (Document) feature;
-            final Feature inner = document.getFeature().get(0);
-            if (inner instanceof Placemark) {
-                final Placemark pm = (Placemark) inner;
-                int x = 0;
+
+        final Path path = file.toPath();
+        final Charset charset = Charset.forName("UTF-8");
+        final List<Instant> rawInstants = new ArrayList<>();
+        final List<float[]> rawLocations = new ArrayList<>();
+        final List<LocationRecord> locations = new ArrayList<>();
+        try (BufferedReader reader = Files.newBufferedReader(path, charset)) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                if (line.matches("<when>\\d\\d\\d\\d-\\d\\d-\\d\\dT\\d\\d:\\d\\d:\\d\\d.\\d\\d\\d-\\d\\d:\\d\\d<\\/when>")) {
+                    rawInstants.add(decodeInstant(line));
+                } else if (line.matches("<gx\\:coord>\\d\\d\\.\\d+\\s\\d\\d\\.\\d+\\s\\d<\\/gx\\:coord>")) {
+                    final String replaceAll = line.replaceAll("[<>\\/\\:]+|[a-z]+", "");
+                    final String[] split = replaceAll.split("\\s");
+                    if (split.length == 3) {
+                        float[] rawLoc = new float[2];
+                        final String floatLatitude = split[0];
+                        final String floatLongitude = split[1];
+                        final float latitude = Float.parseFloat(floatLatitude);
+                        final float longitude = Float.parseFloat(floatLongitude);
+                        rawLoc[0] = latitude;
+                        rawLoc[1] = longitude;
+                        rawLocations.add(rawLoc);
+                    }
+                }
             }
         }
+        catch (final IOException x) {
+            System.err.format("IOException: %s%n", x);
+        }
+    }
+
+    private static Instant decodeInstant(final String line) {
+        final String year = line.substring(6, 10);
+        final String month = line.substring(11, 13);
+        final String day = line.substring(14, 16);
+        final String hour = line.substring(17, 19);
+        final String minute = line.substring(20, 22);
+        final String second = line.substring(23, 25);
+        final String millies = line.substring(26, 29);
+        final String offsetHours = line.substring(29, 32);
+        final String offsetMinutes = line.substring(33, 35);
+
+        final LocalDateTime ldt = LocalDateTime.of(Integer.parseInt(year),
+                Integer.parseInt(month), Integer.parseInt(day),
+                Integer.parseInt(hour), Integer.parseInt(minute),
+                Integer.parseInt(second), Integer.parseInt(millies) * 1000);
+
+        return ldt.toInstant(ZoneOffset.ofHoursMinutes(
+                Integer.parseInt(offsetHours),
+                Integer.parseInt(offsetMinutes)));
     }
 
     private KmlReader() {
